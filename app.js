a0734ae753b4a5a65c08c42ec53100e1617199f1
Timur <teekirienkov@gmail.com>
2020-06-08 14:49:11 +0300

    add vue method

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const path = require('path');
+const app = express();
 
+app.use(express.static(path.resolve(__dirname, 'client'))); // делаем папку статической (чтобы запускались frontend модули js)
 
+app.get('*', (req, res) => { // express будет смотреть за всеми get запросами которые есть
+  res.sendFile(path.resolve(__dirname, 'client', 'index.html')); // express отдает index.html
+});
 
-app.listen(3000, () => console.log(`server start`))
+app.listen(3000, () => console.log(`server start`));
--- client/index.js
+++ client/index.js
@@ -1,2 +1,18 @@
 import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.esm.browser.js';
 
+new Vue({
+  el: '#app', // указываем корневой элемент (selector id)
+  data() {
+    return {
+      form: {
+        name: '',
+        value: ''
+      }
+    }
+  },
+  methods: {
+    createContact() {
+      console.log(this.form)
+    }
+  }
+})
