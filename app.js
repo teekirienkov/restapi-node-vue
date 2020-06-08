633c00bf1efeee56ea6489ea19416836b7565f57
Timur <teekirienkov@gmail.com>
2020-06-08 15:48:44 +0300

    add request async func and loader in html

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+// импровизированная база данных
+const CONTACTS = [
+  {id: 1, name: 'Timur', value: 'admin', marked: false}
+]
+
+// endpoint (url)
+app.get('/api/contacts', (req, res) => {   // /api/contacts - route
+  res.status(200).json(CONTACTS)
+})
+
 app.use(express.static(path.resolve(__dirname, 'client'))); // делаем папку статической (чтобы запускались frontend модули js)
 
 app.get('*', (req, res) => { // express будет смотреть за всеми get запросами которые есть
--- client/index.js
+++ client/index.js
@@ -1,16 +1,24 @@
 import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.esm.browser.js';
 
+Vue.component('loader', {
+  template: `
+    <div style="display: flex; justify-content: center; align-items: center">
+      <div class="spinner-border" role="status">
+        <span class="sr-only">Loading...</span>
+      </div>
+     </div>`
+})
+
 new Vue({
   el: '#app', // указываем корневой элемент (selector id)
   data() {
     return {
+      loading: false,
       form: {
         name: '',
         value: ''
       },
-      contacts: [
-        {id: 1, name: 'Timur', value: '79255962222', marked: false}
-        ]
+      contacts: []
     }
   },
   computed: {
@@ -34,5 +42,33 @@ new Vue({
     removeContact(id) {
       this.contacts = this.contacts.filter(elem => elem.id !== id)
     }
+  },
+  async mounted() {
+    this.loading = true;
+    this.contacts = await request('/api/contacts');
+    this.loading = false;
   }
 })
+
+async function request(url, method = 'GET', data = null) {
+  try {
+    const headers = {};
+    let body
+
+    if (data) {
+      headers['Content-Type'] = 'application/json';
+      body = JSON.stringify(data);
+    }
+
+    const response = await fetch(url, {
+      method: method,
+      headers: headers,
+      body: body
+    });
+
+    return await response.json()
+
+  } catch (e) {
+    console.log(`Error: ${e.message}`)
+  }
+}
